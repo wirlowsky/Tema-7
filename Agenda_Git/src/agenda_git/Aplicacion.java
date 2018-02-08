a6b1092bcf093cedc3ae1e41068e2c6fa9287834
mati <mati@1smx>
2018-02-08 12:05:05 +0100

    v.1

--- Agenda_Git/src/agenda_git/Aplicacion.java
+++ Agenda_Git/src/agenda_git/Aplicacion.java
@@ -5,17 +5,60 @@
  */
 package agenda_git;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mati
  */
 public class Aplicacion {
-
+public static Scanner tc=new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaración de Variables
+       int opt=0,opt2=0,dia=0,mes=0;
+       boolean salir=false;
+       Agenda ag=new Agenda();
+       Pagina pagActual= new Pagina();
+       //Codigo
+       do{
+       switch(opt){
+       case 1:
+           System.out.println("Seleccione dia");
+           dia=tc.nextInt();
+           System.out.println("Seleccione mes");
+           mes=tc.nextInt();
+           pagActual=ag.buscaPagina(dia,mes);
+           switch(opt2){
+               case 1:
+                   break;
+               case 2:
+                   break;
+               case 3:
+                   break;
+               case 4:
+                   break;
+               default:
+                   System.out.println("Opcion no especificada");
+                   break;
+           
+           
+           }//Cierra el subSwitch
+               break;
+               
+       case 0:
+           salir=true;
+           break;
+       
+       
+       
+       
+       }//Cierra el switch
+       
+       
+       }while(!salir);
     }
     
 }
